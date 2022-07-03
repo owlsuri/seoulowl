0e05b617ff71159531868ff75d4c105fe377551a
owlsuri <imsimple2563@gmail.com>
2022-07-03 18:43:11 +0900

    수정:경로

--- src/commons/apollo/index.tsx
+++ src/commons/apollo/index.tsx
@@ -41,7 +41,7 @@ export default function ApolloSetting(props) {
   });
 
   const uploadLink = createUploadLink({
-    uri: "https://backend06.codebootcamp.co.kr/graphql",
+    uri: "https://backend06.codebootcamp.co.kr/graphql11",
     headers: { Authorization: `Bearer ${accessToken}` },
     credentials: "include",
   });
