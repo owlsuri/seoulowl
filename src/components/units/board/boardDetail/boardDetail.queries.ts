464e19cbb72eddce9faef6b1b91f3412dbed8f46
owlsuri <imsimple2563@gmail.com>
2022-07-05 15:24:50 +0900

    수정

--- src/components/units/board/boardDetail/boardDetail.queries.ts
+++ src/components/units/board/boardDetail/boardDetail.queries.ts
@@ -1,5 +1,26 @@
 import { gql } from "@apollo/client";
 
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      createdAt
+    }
+  }
+`;
+
 export const DELETE_BOARD = gql`
   mutation deleteBoard($boardId: ID!) {
     deleteBoard(boardId: $boardId)
